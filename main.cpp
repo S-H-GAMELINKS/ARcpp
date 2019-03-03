04f9c024ad1cebb2f95d71e094ad46be9fa06f5a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-03 22:01:36 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <httplib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+const std::string load_assets(const std::string& path) {
+
+    std::ifstream file(path.c_str(), std::ios::in);
+
+    std::stringstream stream;
+
+    stream << file.rdbuf();
+
+    file.close();
+
+    std::string assets(stream.str());
+
+    return assets;
+}
+
+int main() {
+
+    httplib::Server svr;
+
+    std::string body = load_assets("./index.html");
+
+    svr.Get("/", [&](const httplib::Request& req, httplib::Response& res) {
+        res.set_content(body, "text/html");
+    });
+
+    svr.listen("localhost", 3000);
+
+    return 0;
+}
