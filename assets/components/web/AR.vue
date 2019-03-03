727f25b3bd79450a29ce6e8604f5a62db8e7ac7f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-03 23:05:05 +0900

    add AR.vue

---
+++ assets/components/web/AR.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <a-scene embedded arjs>
+  	        <a-marker preset="hiro">
+                <a-box position='0 0.5 0' material='color: black;'></a-box>
+  	        </a-marker>
+  	        <a-entity camera></a-entity>
+        </a-scene>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
